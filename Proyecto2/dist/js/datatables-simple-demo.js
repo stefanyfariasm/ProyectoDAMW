a7124b7c5d245e931b4b30df42fdd0058f517cc7
stefanyfariasm <snfarias@espol.edu.ec>
2022-07-15 04:18:25 -0500

    realización de la tabla de las marcas de telefonos

--- Proyecto2/dist/js/datatables-simple-demo.js
+++ Proyecto2/dist/js/datatables-simple-demo.js
@@ -1,9 +1,70 @@
 window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
+    
+    function cargarDatos(elem){
+        let id = elem['brand_id'];
+        let marca = elem['brand_name'];
+        let marcagene = elem['brand_slug'];
+        let numero = elem['device_count'];
+             
+        let plantilla = `<tr>
+            <td>${id}</td>
+            <td>${marca}</td>
+            <td>${marcagene}</td>
+            <td>${numero}</td>        
+            </tr>`;
+            tablaMarcas.innerHTML += plantilla;
     }
+    
+    fetch("https://api-mobilespecs.azharimm.site/v2/brands")
+        .then(response => response.json())
+        .then(data => {
+            let datos = data['data'];
+            let  tablaMarcas = document.getElementById('tablaMarcas');
+            let seleccion= document.querySelector('div.seleccion > select');
+            for(var elem of datos){
+                let id = elem['brand_id'];
+                let marca = elem['brand_name'];
+                let marcagene = elem['brand_slug'];
+                let numero = elem['device_count'];
+             
+                let plantilla = `<tr>
+                    <td>${id}</td>
+                    <td>${marca}</td>
+                    <td>${marcagene}</td>
+                    <td>${numero}</td>
+                      
+                    </tr>`;
+                tablaMarcas.innerHTML += plantilla;
+
+                let plantillaMarcas = `<option value= "${marca}">${marca}</option>`;
+            
+                seleccion.innerHTML += plantillaMarcas;
+                
+            }            
+            seleccion.addEventListener("change",function(){
+                tablaMarcas.innerHTML = '';
+
+                for(var elem of datos){
+                    if(seleccion.value == "Seleccionar marca"){
+                        cargarDatos(elem);
+                    }
+                    else if(elem['brand_name'] == select.value){
+                        let id = elem['brand_id'];
+                        let marca = elem['brand_name'];
+                        let marcagene = elem['brand_slug'];
+                        let numero = elem['device_count'];
+                
+                        let plantilla = `<tr>
+                            <td>${id}</td>
+                            <td>${marca}</td>
+                            <td>${marcagene}</td>
+                            <td>${numero}</td>                       
+                            </tr>`;
+                        tablaMarcas.innerHTML += plantilla;
+                    }
+                }
+            })
+            
+        }).catch(console.error);
+      
 });
